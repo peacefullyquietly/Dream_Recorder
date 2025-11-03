dd61cb454a47010e014c4e81d99103643d2aba5e
peacefullyquietly <pjh73015@gmail.com>
2025-11-03 21:34:31 +0900

    feat : Create React App으로 폴더 초기화

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DreamRecorder from "./front.jsx"; // DreamRecorder.js 파일 경로
+
+function App() {
+  return (
+    <div className="App">
+      <DreamRecorder />
+    </div>
+  );
+}
+
+export default App;
